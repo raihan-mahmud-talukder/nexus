7d0a97a6a1687ea87bc3892be101a6fe1bfa181d
Raihan <msr.raihan0mahmud@gmail.com>
2026-04-05 13:43:25 +0600

    footer setup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,20 @@
 export const Footer = () => {
-    return <footer>FOOTER</footer>
+    return (
+        <footer>
+            <nav>
+                <div>
+                    <img src="images/logo.png" alt="logo" />
+                </div>
+                <span>Nexus</span>
+            </nav>
+            <p>
+                © 2025 Nexus Creative Agency. All rights reserved.
+            </p>
+            <ul>
+                <li>Twitter</li>
+                <li>Instagram</li>
+                <li>Dribble</li>
+            </ul>
+        </footer>
+    )
 }
