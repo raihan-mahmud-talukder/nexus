54c68da302c76b7f1940390353a9480052bb6767
Raihan <msr.raihan0mahmud@gmail.com>
2026-04-05 11:57:11 +0600

    project setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+export const Header = () => {
+    return (
+        <header>
+            <div>
+                <div className="logo">
+                    <img src="SVG.png" alt="logo" />
+                </div>
+                <span>Nexus</span>
+            </div>
+            <ul className="">
+                <li>Work</li>
+                <li>Studio</li>
+                <li>Services</li>
+                <li>Insights</li>
+            </ul>
+            <span>
+                <a href="">Let's Talk</a>
+            </span>
+        </header>
+    )
+}
