b373f5e60082b3d9eb48df33e8787295c1c108e5
Raihan <msr.raihan0mahmud@gmail.com>
2026-04-05 14:10:57 +0600

    caption

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,7 +1,23 @@
 export const Hero = () => {
     return (
-        <section>
-            <h1>HERO</h1>
+        <section className="hero">
+            <div className="caption">
+                <img src="images/star.png" alt="star" />
+                <span>Award Winning Design Agency</span>
+            </div>
+            <h1 className="brand">We Build Iconic Brands</h1>
+            <p>A creative agency designing digital experiences for
+                tomorrow. We blend high-end aesthetics with
+                powerful strategy to craft legendary digital
+                products.
+            </p>
+            <div>
+                <button>
+                    <span>View Projects</span>
+                    <img src="images/arrow.png" alt="right arrow" />
+                </button>
+                <button>Our Expertise</button>
+            </div>
         </section>
     )
 }
