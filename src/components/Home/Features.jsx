d6cbca95e710cd45832afc6e5acbde6cebd7e2ba
raihan-mahmud-talukder <msr.raihan0mahmud@gmail.com>
2026-04-06 16:00:06 +0600

    features

--- src/components/Home/Features.jsx
+++ src/components/Home/Features.jsx
@@ -1,7 +1,32 @@
+import { features } from "../../data/features"
+
 export const Features = () => {
     return (
-        <section>
-            <h1>FEATURE</h1>
+        <section className="feature">
+            <h2>Featured Work</h2>
+            <p>
+                Explore our latest collection of premium digital products, immersive 3D experiences, and iconic brand identities.
+            </p>
+            <div>
+                {features.map((item, index) => <Feature features={item} key={index} />)}
+            </div>
         </section>
+    )
+}
+
+const Feature = ({ features }) => {
+    return (
+        <aside>
+            <div className="container">
+                <img src={features.imgage} alt={features.title} />
+            </div>
+            <div className="background">
+                <div>
+                    <h3>{features.title}</h3>
+                    <span>{features.caption}</span>
+                </div>
+                <img src='images/features/arrow.png' />
+            </div>
+        </aside>
     )
 }
--- src/data/features.js
+++ src/data/features.js
@@ -3,36 +3,36 @@ export const features = [
         id: 1,
         title: 'Lumina Web3',
         caption: 'Digital Product & 3D',
-        imgage: '/images/features/.jpg'
+        imgage: '/images/features/lumina-web3.png'
     },
     {
         id: 2,
         title: 'Vanguard',
         caption: 'Mobile Application',
-        imgage: '/images/features/.jpg'
+        imgage: '/images/features/vanguard.png'
     },
     {
         id: 3,
         title: 'Kroma Studio',
         caption: 'E-Commerce',
-        imgage: '/images/features/.jpg'
+        imgage: '/images/features/kroma-studio.png'
     },
     {
         id: 4,
         title: 'Aether OS',
         caption: 'Interface Design',
-        imgage: '/images/features/.jpg'
+        imgage: '/images/features/aether-os.png'
     },
     {
         id: 5,
         title: 'Nova Synth',
         caption: 'Interface Design',
-        imgage: '/images/features/.jpg'
+        imgage: '/images/features/nova-synth.png'
     },
     {
         id: 6,
         title: 'Chronos Wear',
         caption: 'Interaction Design',
-        imgage: '/images/features/.jpg'
+        imgage: '/images/features/chronos-wear.png'
     },
 ]
