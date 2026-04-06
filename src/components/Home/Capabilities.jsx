cdebe93181883118ae83a37f8677b548fc8c2901
raihan-mahmud-talukder <msr.raihan0mahmud@gmail.com>
2026-04-06 09:39:24 +0600

    capabilities

--- src/components/Home/Capabilities.jsx
+++ src/components/Home/Capabilities.jsx
@@ -1,3 +1,5 @@
+import { capabilities } from "../../data/capabilities"
+
 export const Capabilities = () => {
     return (
         <section className="capability">
@@ -5,9 +7,21 @@ export const Capabilities = () => {
             <p>
                 We are a multi-disciplinary studio leveraging future- ready technologies to craft industry-leading platforms.
             </p>
-            <article>
-                
-            </article>
+            <div>
+            {capabilities.map((item, index) => {
+                return <Capability key={index} capability={item} />
+            })}
+            </div>
         </section>
     )
+}
+
+const Capability = ({capability}) => {
+    return (
+        <article>
+            <img src={capability.icon} alt={capability.title} />
+            <h4>{capability.title}</h4>
+            <p>{ capability.description}</p>
+        </article>
+)
 }
--- src/data/capabilities.js
+++ src/data/capabilities.js
@@ -1,19 +1,19 @@
-export const apabilities = [
+export const capabilities = [
     {
         id: 1,
-        // icon: '/images/capablities/.png',
+        icon: '/images/capabilities/layout.png',
         title: 'Digital Products',
         description: 'End-to-end product design and engineering. We build high-performance web and mobile applications with impeccable UI/UX.'
     },
     {
         id: 2,
-        // icon: '/images/capablities/.png',
+        icon: '/images/capabilities/cube.png',
         title: '3D & Motion',
         description: 'Cinematic 3D rendering and immersive animations that elevate your brand narrative and captivate your audience.'
     },
     {
         id: 3,
-        // icon: '/images/capablities/.png',
+        icon: '/images/capabilities/hexagon.png',
         title: 'Brand Strategy',
         description: 'Data-driven strategy to position your brand as a market leader. We create cohesive identities that scale.'
     },
