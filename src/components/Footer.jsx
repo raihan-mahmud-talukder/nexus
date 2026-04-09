9b33ccfc97b0162921efa898ba5c4b9b0a847f19
raihan-mahmud-talukder <msr.raihan0mahmud@gmail.com>
2026-04-09 22:43:35 +0600

    footer mobile responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,39 @@
 export const Footer = () => {
     return (
-        <footer className="w-full">
-            <div className="flex items-center gap-2">
+        <footer className="
+            flex flex-col
+            gap-2
+            justify-center
+            border-t border-t-[#FFFFFF0A] border-solid
+            pt-5 pb-5 px-4
+            ">
+            <div className="
+                flex items-center gap-1
+                justify-center">
                 <img
-                    className="w-6 h-6 rounded-md"
+                    className="w-4 h-4 rounded-sm"
                     src="images/logo.png" alt="logo" />
                 <span
                     className="
-                    text-2xl
+                    text-sm
                     font-extrabold
                     uppercase
                     tracking-tight
                     align-middle
                     ">Nexus</span>
             </div>
-            <p>
+            <p className="
+                font-normal
+                text-xs
+                align-middle
+                text-center
+                text-[#8A98A6]">
                 © 2025 Nexus Creative Agency. All rights reserved.
             </p>
-            <ul>
+            <ul className="
+                flex gap-6
+                text-sm font-medium
+                justify-center">
                 <li>Twitter</li>
                 <li>Instagram</li>
                 <li>Dribble</li>
