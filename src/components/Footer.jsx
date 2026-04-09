b9f774faa661f513baee0e9a104bc0fcbea15f6d
raihan-mahmud-talukder <msr.raihan0mahmud@gmail.com>
2026-04-09 19:24:19 +0600

    footer logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,18 @@
 export const Footer = () => {
     return (
-        <footer className="">
-            <div className="">
-                <img className="" src="images/logo.png" alt="logo" />
-                <span>Nexus</span>
+        <footer className="w-full">
+            <div className="flex items-center gap-2">
+                <img
+                    className="w-6 h-6 rounded-md"
+                    src="images/logo.png" alt="logo" />
+                <span
+                    className="
+                    text-2xl
+                    font-extrabold
+                    uppercase
+                    tracking-tight
+                    align-middle
+                    ">Nexus</span>
             </div>
             <p>
                 © 2025 Nexus Creative Agency. All rights reserved.
