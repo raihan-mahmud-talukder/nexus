d1ae538ac1074112583f84d7405d07bd149ad9ad
raihan-mahmud-talukder <msr.raihan0mahmud@gmail.com>
2026-04-12 16:50:05 +0600

    capabilities responsive

--- src/components/Home/Capabilities.jsx
+++ src/components/Home/Capabilities.jsx
@@ -2,12 +2,24 @@ import { capabilities } from "../../data/capabilities"
 
 export const Capabilities = () => {
     return (
-        <section className="capability">
-            <h2>Our Capabilities</h2>
-            <p>
+        <section className="
+            px-4 py-10 md:py-40
+        ">
+            <h2 className="
+                text-3xl md:5xl
+                font-bold tracking-tighter
+                text-center align-middle
+            ">Our Capabilities</h2>
+            <br />
+            <p className="
+                max-w-lg px-2 text-lg font-normal text-center m-auto
+            ">
                 We are a multi-disciplinary studio leveraging future- ready technologies to craft industry-leading platforms.
             </p>
-            <div>
+            <br /><br />
+            <div className="
+                flex flex-col gap-6 md:flex-row
+            ">
             {capabilities.map((item, index) => {
                 return <Capability key={index} capability={item} />
             })}
@@ -18,10 +30,23 @@ export const Capabilities = () => {
 
 const Capability = ({capability}) => {
     return (
-        <article>
-            <img src={capability.icon} alt={capability.title} />
-            <h4>{capability.title}</h4>
-            <p>{ capability.description}</p>
+        <article className="
+            pt-5 px-5 pb-4
+            md:pt-10 md:px-10 md:pb-8
+            flex flex-col justify-start gap-2
+            rounded-lg md:rounded-xl
+            border border-white border-opacity-10
+            backdrop-blur-xl bg-[]
+        ">
+            <img className="
+                w-16 h-16 rounded-lg
+            " src={capability.icon} alt={capability.title} />
+            <h4 className="
+                font-semibold text-2xl
+            ">{capability.title}</h4>
+            <p className="
+                text-base font-normal text-[#8a98a6]
+            ">{capability.description}</p>
         </article>
 )
 }
